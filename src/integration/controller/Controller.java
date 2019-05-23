26f87fd9649f95a71e62a86d862b7fa6f4965d32
stacey <stacey.baytalyuk@gmail.com>
2019-05-23 23:52:01 +0300

    3*sqrt(x) 1-9

--- src/integration/controller/Controller.java
+++ src/integration/controller/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import static java.lang.Math.sqrt;
 
 public class Controller {
     @FXML
@@ -19,13 +20,12 @@ public class Controller {
     @FXML
     private  Text timeText;
 
-    private double totalResult = 0;
-    private int finishedThreadCount = 0;
-    private final double a = 0;
-    private final double b = Math.PI;
-    //int n = 1000_000_000;
-    private int n = 1_000_000;
-    private int threads = 20;
+    private final double a = 1;
+    private final double b = 9;
+    private double totalResult;
+    private int finishedThreadCount;
+    private int n;
+    private int threads;
 
     public void onOk() {
         boolean empty = (intervalsTextField.getText() == null || intervalsTextField.getText().isEmpty())
@@ -38,9 +38,7 @@ public class Controller {
                 if ( (n < 1) || (threads < 1) ) {
                     showAlert("Кількість потоків та інтервалів має бути доданою");
                 } else {
-                    calculateButton.setDisable(true);
                     calculate();
-                    calculateButton.setDisable(false);
                 }
             } else {
                 showAlert("Будь ласка, введіть усі поля");
@@ -48,19 +46,15 @@ public class Controller {
         } catch (NumberFormatException e) {
             showAlert("Неправильний формат вводу");
         }
-
     }
 
     private void calculate() {
-        integralText.setText("Розрахунок...");
-        timeText.setText("");
-
         totalResult = 0;
         finishedThreadCount = 0;
         double delta = (b - a) / threads;
         long start = System.currentTimeMillis();
         for (int i = 0; i < threads; i++) {
-            ThreadIntegralCalculator t = new ThreadIntegralCalculator(this, a + i * delta, a + (i + 1) * delta, n / threads, Math::sin);
+            ThreadIntegralCalculator t = new ThreadIntegralCalculator(this, a+i*delta, a+(i+1)*delta, n/threads, (x) -> 3*sqrt(x));
             new Thread(t).start();
         }
 
